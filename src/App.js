fca860c2598331c2f08f08174ad16b314550b0f6
xavier <facqueur.xavier@sfr.fr>
2020-06-13 16:41:34 +0200

    move data from Week to App + better fetch

--- src/App.js
+++ src/App.js
@@ -4,30 +4,88 @@ import './App.css';
 // import moment from './momentjs/moment.min.js'
 
 class App extends Component {
-  
   state = {
     min_date:'2019-06-24',
-    max_date:'2019-06-30'
+    max_date:'2019-06-30',
+    weekActivities: [],
+    data:[],
+    loaded: false
+  }
+
+  async loadState (dataOffer) {
+    let weekActivities = [[],[],[],[],[],[],[]]
+
+    //set activities for each day
+    for (let i = 0; i < dataOffer.length; i++){
+        const day = new Date(dataOffer[i].date_start).getDay()
+        
+        //find the right coach name
+        const coach = await fetch('https://back.staging.bsport.io/api/v1/coach/?company=6&id__in=' + dataOffer[i].coach)
+        const dataCoach = await coach.json()
+        let coachName = dataCoach.results[0].user.name
+
+        const activity = {
+          activity: dataOffer[i].activity,
+          date_start: dataOffer[i].date_start,
+          coach: coachName
+        }
+        console.log(day)
+        weekActivities[day].push(activity)
+      }
+
+      this.setState({weekActivities : weekActivities})
+  }
+
+  async componentDidMount() {
+      const response = await fetch('https://back.staging.bsport.io/api/v1/offer/?min_date=' + this.state.min_date + '&max_date=' + this.state.max_date + '&company=6')
+      const data = await response.json()
+      this.loadState(data.results)
+      this.state.loaded = true;
+  }
+
+  async componentDidUpdate(prevProps, prevState){
+    if (prevState.min_date !== this.state.min_date)
+    {
+      const response = await fetch('https://back.staging.bsport.io/api/v1/offer/?min_date=' + this.state.min_date + '&max_date=' + this.state.max_date + '&company=6')
+      const data = await response.json()
+      this.loadState(data.results, this.state.dataCoach)
+      this.state.loaded = true;
+    }
   }
 
   previousWeek = () => {
-    this.setState({min_date: '2019-06-17', max_date: '2019-06-24'})
+    if (this.state.min_date !== '2019-06-17')
+      this.setState({min_date: '2019-06-17', max_date: '2019-06-24', loaded:false})
   }
 
   nextWeek = () => {
-    this.setState({min_date: '2019-06-24', max_date: '2019-06-30'})
+    if (this.state.min_date !== '2019-06-24')
+      this.setState({min_date: '2019-06-24', max_date: '2019-06-30', loaded:false})
   }
 
   render()
   {
+    if(this.state.loaded === false)
+      return (
+        <div className="App">
+        <h1 style={{backgroundColor:'lightgreen', margin:'auto'}}> Calendrier </h1>
+        <hr style={{margin:'auto'}}></hr>
+        <div>Loading...</div>
+        </div>)
+
+    const activities = []
+    for (let i  = 1; i < this.state.weekActivities.length; i++){
+      activities.push(this.state.weekActivities[i])
+    }
+    activities.push(this.state.weekActivities[0])
+    
     return (
       <div className="App">
         <h1 style={{backgroundColor:'lightgreen', margin:'auto'}}> Calendrier </h1>
         <hr style={{margin:'auto'}}></hr>
-        <Week min_date={this.state.min_date} max_date={this.state.max_date} previousWeek={this.previousWeek} nextWeek={this.nextWeek}></Week>
+        <Week activities={activities} min_date={this.state.min_date} max_date={this.state.max_date} previousWeek={this.previousWeek} nextWeek={this.nextWeek} ></Week>
       </div>
     );
   }
 }
-
 export default App;
--- src/Week.js
+++ src/Week.js
@@ -4,123 +4,20 @@ import Activity from './Activity'
 class Week extends Component {
 
     state = {
-        mondayActivity: [],
-        tuesdayActivity: [],
-        wednesdayActivity: [],
-        thursdayActivity: [],
-        fridayActivity: [],
-        saturdayActivity: [],
-        sundayActivity: [],
         day: '-1',
-        data: [],
-        dataCoach: []
-    }
-
-    filterData = (data) => {
-        let minDate = new Date(this.props.min_date)
-        let maxDate = new Date(this.props.max_date)
-        return data.results
-        .filter(offer => offer.company === 6)
-        .filter(function(offer) { 
-            let date = new Date(offer.date_start.substring(0, 10))
-            return date >= minDate && date < maxDate;
-        })
-    }
-
-    loadState = (dataOffer, dataCoach_) => {
-
-        const dataCoach = dataCoach_.results
-
-        let mondayActivity = []
-        let tuesdayActivity = []
-        let wednesdayActivity = []
-        let thursdayActivity = []
-        let fridayActivity = []
-        let saturdayActivity = []
-        let sundayActivity = []
-
-        //set activities for each day
-        for (let i = 0; i < dataOffer.length; i++){
-            const day = new Date(dataOffer[i].date_start).getDay()
-            console.log(day)
-            let coachName = ""
-            //find the right coach name
-            for (let j = 0; j < dataCoach.length; j++){
-                if (dataCoach[j].id === dataOffer[i].coach)
-                    coachName = dataCoach[j].user.name
-            }
-            
-            const activity = {
-                activity: dataOffer[i].activity,
-                date_start: dataOffer[i].date_start,
-                coach: coachName
-            }
-            console.log(activity.coach)
-            switch(day){
-                case 0:
-                    sundayActivity.push(activity)
-                    break
-                case 1:
-                    mondayActivity.push(activity)
-                    break
-                case 2:
-                    tuesdayActivity.push(activity)
-                    break
-                case 3:
-                    wednesdayActivity.push(activity)
-                    break
-                case 4:
-                    thursdayActivity.push(activity)
-                    break
-                case 5:
-                    fridayActivity.push(activity)
-                    break
-                case 6:
-                    saturdayActivity.push(activity)
-                    break
-                default:
-                    console.log("shouldn't happen")  
-            }
-        }
-        this.setState({mondayActivity: mondayActivity,
-            tuesdayActivity: tuesdayActivity,
-            wednesdayActivity: wednesdayActivity,
-            thursdayActivity: thursdayActivity,
-            fridayActivity: fridayActivity,
-            saturdayActivity: saturdayActivity,
-            sundayActivity: sundayActivity})
-    }
-
-    async componentDidMount() {
-        const response = await fetch('https://back.staging.bsport.io/api/v1/offer')
-        const data = await response.json()
-        const coach = await fetch('https://back.staging.bsport.io/api/v1/coach/?page_size=500')
-        const dataCoach = await coach.json()
-        
-        this.setState({data: data, dataCoach: dataCoach})
-        let dataOffer = this.filterData(data)
-        this.loadState(dataOffer, dataCoach)
-    }
-
-    componentDidUpdate(prevProps, prevState){
-        if (prevProps !== this.props)
-        {
-            let dataOffer = this.filterData(this.state.data)
-            this.loadState(dataOffer, this.state.dataCoach)
-        }
     }
 
     daysToDisplay = (activity, index, days) => {
         if(this.state.day === '-1' || this.state.day === index.toString())
         {
-        return (
-        <div key={index}>
-            <p style={{backgroundColor: '#FACD5D', width:'240px', padding:'10px', margin : 'auto', borderRadius:'5px', border: 'solid 1px black'}}>{days[index]}</p>
-            {
-            activity.map((elem, index) => {
-                return (<div key={index}> <Activity date_start={elem.date_start} coach={elem.coach} activity={elem.activity}/> </div>)
-            })}
-        </div>)
+            return (
+            <div key={index}>
+                <p style={{backgroundColor: '#FACD5D', width:'240px', padding:'10px', margin : 'auto', borderRadius:'5px', border: 'solid 1px black'}}>{days[index]}</p>
+                {
+                activity.map((elem, index) => {
+                    return (<div key={index}> <Activity date_start={elem.date_start} coach={elem.coach} activity={elem.activity}/> </div>)
+                })}
+            </div>)
         }
     }
 
@@ -129,12 +26,9 @@ class Week extends Component {
     }
     
     render(){
-        const activities = [this.state.mondayActivity, this.state.tuesdayActivity, this.state.wednesdayActivity, this.state.thursdayActivity,
-            this.state.fridayActivity, this.state.saturdayActivity, this.state.sundayActivity]
         const days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
         return (
             <>
-
             <div style={{display:'flex', justifyContent:'space-between'}}>
                 <button onClick={this.props.previousWeek}> previous week </button>
                 <div> Semaine du {this.props.min_date}</div>
@@ -153,7 +47,7 @@ class Week extends Component {
                     <option value='6'>Dimanche</option>
                 </select>
                 {
-                activities.map((activity, index) => {
+                this.props.activities.map((activity, index) => {
                     return this.daysToDisplay(activity, index, days)}
                     )
                 }
